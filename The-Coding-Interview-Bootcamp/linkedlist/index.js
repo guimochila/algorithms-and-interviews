b4c9aad9a3804e921da8fc4c547837a0d2a6e9b7
Guilherme <gui.mochila@gmail.com>
2018-11-19 14:12:21 -0200

    Symbol.iterator added to the linkedList.

--- The-Coding-Interview-Bootcamp/linkedlist/index.js
+++ The-Coding-Interview-Bootcamp/linkedlist/index.js
@@ -154,6 +154,15 @@ class LinkedList {
       index++;
     }
   }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
