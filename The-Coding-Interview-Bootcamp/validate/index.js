85b38fa82b3c935fb7f0008bc661973e7d2c481d
Guilherme <gui.mochila@gmail.com>
2018-11-24 09:19:53 -0200

    BST validation added.🔥

---
+++ The-Coding-Interview-Bootcamp/validate/index.js
@@ -0,0 +1,39 @@
+// --- Directions
+// Given a node, validate the binary search tree,
+// ensuring that every node's left hand child is
+// less than the parent node's value, and that
+// every node's right hand child is greater than
+// the parent
+
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
+
+// Alternative method
+
+function validate1(node, min = null, max = null) {
+  if ((max && node.data > max) || (min && node.data < min)) return false;
+
+  if (node.left) return validate(node.left, min, node.data);
+  if (node.right) return validate(node.right, node.data, max);
+
+  return true;
+}
+
+module.exports = validate1;
