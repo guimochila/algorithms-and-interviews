cad086d927d709449a41c4d256ec5ced28f90594
Guilherme <gui.mochila@gmail.com>
2018-11-25 10:01:00 -0200

    Bubble sort algorithm added.

---
+++ The-Coding-Interview-Bootcamp/sorting/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+function selectionSort(arr) {}
+
+function mergeSort(arr) {}
+
+function merge(left, right) {}
+
+module.exports = { bubbleSort, selectionSort, mergeSort };
