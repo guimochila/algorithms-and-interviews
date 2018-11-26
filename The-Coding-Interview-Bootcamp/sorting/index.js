f1c4b64fa91d733f2473a2a25782fd11ab077b26
Guilherme <gui.mochila@gmail.com>
2018-11-26 11:55:14 -0200

    MergeSort algorithm added.

--- The-Coding-Interview-Bootcamp/sorting/index.js
+++ The-Coding-Interview-Bootcamp/sorting/index.js
@@ -33,8 +33,31 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
-function merge(left, right) {}
+  const centerIndex = Math.floor(arr.length / 2);
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+  const left = arr.slice(0, centerIndex);
+  const right = arr.slice(centerIndex);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
