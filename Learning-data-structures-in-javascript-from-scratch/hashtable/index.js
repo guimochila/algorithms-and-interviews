d3c938eb47345d00832657439af246629b60daa7
Guilherme <gui.mochila@gmail.com>
2018-12-02 11:31:09 -0200

    Hash and insert methods added.

--- Learning-data-structures-in-javascript-from-scratch/hashtable/index.js
+++ Learning-data-structures-in-javascript-from-scratch/hashtable/index.js
@@ -12,7 +12,41 @@ class HashTable {
     this.buckets = Array(size);
     this.numberOfBuckets = this.buckets.length;
   }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+
+    return total % this.numberOfBuckets;
+  }
+
+  insert(key, value) {
+    const index = this.hash(key);
+
+    if (!this.buckets[index]) {
+      this.buckets[index] = new HashNode(key, value);
+    } else if (this.buckets[index].key === key) {
+      this.buckets[index].value = value;
+    } else {
+      let currentNode = this.buckets[index];
+
+      while (currentNode.next) {
+        if (currentNode.next.key === key) {
+          currentNode.next.value = value;
+          return;
+        }
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = new HashNode(key, value);
+    }
+  }
 }
 
 const ht = new HashTable(30);
+ht.insert('Engineer', 'Javascript');
+ht.insert('Engineer', 'React');
+ht.insert('Developer', 'Google');
 console.log(ht);
