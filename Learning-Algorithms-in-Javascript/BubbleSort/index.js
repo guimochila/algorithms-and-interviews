5f0632b55d741bb74e9b7a323f6ec07256920a6c
Guilherme <gui.mochila@gmail.com>
2018-12-08 11:52:39 -0200

    BubbleSort added.

---
+++ Learning-Algorithms-in-Javascript/BubbleSort/index.js
@@ -0,0 +1,24 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+function bubbleSort2(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort2([5, 3, 8, 2, 1, 4]));
